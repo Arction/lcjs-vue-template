108b15d9f80f5d605cfcd5adbda1b0072d801705
Niilo Keinänen <niilo.keinanen@lightningchart.com>
2024-05-22 15:21:06 +0300

    Move to @lightningchart NPM organization. Update to latest LCJS

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,55 +1,55 @@
 <template>
-     <div :id="chartId" class="fill"></div>
+  <div :id="chartId" class="fill"></div>
 </template>
 
 <script>
-import { lightningChart  } from '@arction/lcjs'
+import { lightningChart } from "@lightningchart/lcjs";
 
 export default {
-  name: 'ChartData',
-  props: ['points'],
-   data() {
+  name: "ChartData",
+  props: ["points"],
+  data() {
     // Add the chart to the data in a way that Vue will not attach it's observers to it.
-    // If the chart variable would be added in the return object, Vue would attach the observers and 
+    // If the chart variable would be added in the return object, Vue would attach the observers and
     // every time LightningChart JS made a change to any of it's internal variables, vue would try to observe the change and update.
     // Observing would slow down the chart a lot.
-    this.chart = null
+    this.chart = null;
     return {
       chartId: null,
-    }
+    };
   },
   methods: {
     createChart() {
       // Create chartXY
-      this.chart = lightningChart().ChartXY({container: `${this.chartId}`})
+      this.lc = lightningChart();
+      this.chart = this.lc.ChartXY({ container: `${this.chartId}` });
       // Set chart title
-      this.chart.setTitle('Getting Started')
+      this.chart.setTitle("Getting Started");
       // Add line series to the chart
-      const lineSeries = this.chart.addLineSeries()
+      const lineSeries = this.chart.addLineSeries();
       // Set stroke style of the line
-      lineSeries.setStrokeStyle((style) => style.setThickness(5))
+      lineSeries.setStrokeStyle((style) => style.setThickness(5));
       // Add data points to the line series
-      lineSeries.add(this.points)
-  }
+      lineSeries.add(this.points);
+    },
   },
   beforeMount() {
     // Generate random ID to us as the containerId for the chart and the target div id
-    this.chartId = Math.trunc(Math.random() * 1000000)
+    this.chartId = Math.trunc(Math.random() * 1000000);
   },
-   mounted() {
-    // Chart can only be created when the component has mounted the DOM because 
+  mounted() {
+    // Chart can only be created when the component has mounted the DOM because
     // the chart needs the element with specified containerId to exist in the DOM
-    this.createChart()
+    this.createChart();
   },
-   beforeUnmount() {
+  beforeUnmount() {
     // "dispose" should be called when the component is unmounted to free all the resources used by the chart
-    this.chart.dispose()
-  }
-}
-
+    this.lc.dispose();
+  },
+};
 </script>
 <style scoped>
-  .fill {
-    height: 100%;
-  }
+.fill {
+  height: 100%;
+}
 </style>
